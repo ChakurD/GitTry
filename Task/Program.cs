3f690a7c50f9a2132fb3f0506c1917b68774c5bf
ChakurD <cdgameboy98@gmail.com>
2023-02-01 17:24:02 +0300

    1Try

---
+++ Task/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = Console.ReadLine();
+            string b = Console.ReadLine();
+            Console.WriteLine("Результат сложения: ");
+            Console.WriteLine(int.Parse(a) + int.Parse(b));
+        }
+    }
+}
