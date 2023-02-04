eb397fe001dc74f7bf3d7e87daa91219d37657b7
ChakurD <cdgameboy98@gmail.com>
2023-02-04 19:20:33 +0300

    SecondHomeWork

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,102 @@
+﻿namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите номер месяца от 1 до 12:");
+            int MonthNum = int.Parse(Console.ReadLine());
+            switch (MonthNum) 
+            {
+                case 12:
+                case 1:
+                case 2:
+                    Console.WriteLine($"{MonthNum} соответствует Зиме");
+                    break;
+                case 3:
+                case 4:
+                case 5:
+                    Console.WriteLine($"{MonthNum} соответствует Весне");
+                    break;
+                case 6:
+                case 7:
+                case 8:
+                    Console.WriteLine($"{MonthNum} соответствует Лету");
+                    break;
+                case 9:
+                case 10:
+                case 11:
+                    Console.WriteLine($"{MonthNum} соответствует Осени");
+                    break;
+            }
+
+            //--------------------------------------------------------------------------------
+
+            Console.WriteLine("Введите номер месяца от 1 до 12:");
+            int SecMonthNum = Convert.ToInt32(Console.ReadLine());
+            if (SecMonthNum <= 2 || SecMonthNum == 12)
+            {
+                Console.WriteLine($"{SecMonthNum} соответствует Зиме");
+            }
+            else if (SecMonthNum <= 5)
+            {
+                Console.WriteLine($"{SecMonthNum} соответствует Весне");
+            }
+            else if (SecMonthNum <= 8)
+            {
+                Console.WriteLine($"{SecMonthNum} соответствует Лету");
+            }
+            else if (SecMonthNum <= 11)
+            {
+                Console.WriteLine($"{SecMonthNum} соответствует Осени");
+            }
+
+            //--------------------------------------------------------------------------------
+
+            Console.WriteLine("Введите число: ");
+            int ParNum = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(ParNum % 2 == 0 ? $"{ParNum} Четное" : $"{ParNum} Не четное");
+
+            //--------------------------------------------------------------------------------
+
+            Console.WriteLine("Введите Температуру:");
+            int t = Convert.ToInt32(Console.ReadLine());
+            if (t > -5) { Console.WriteLine("Тепло"); }
+            if (t <= -5 && t > -20) { Console.WriteLine("Нормально"); }
+            if (t <= -20) { Console.WriteLine("Холодно"); }
+
+            //--------------------------------------------------------------------------------
+
+            Console.WriteLine("Введите число от 1 до 21 :");
+            int Color = Convert.ToInt32(Console.ReadLine());
+            if (Color <= 3) 
+            {
+                Console.WriteLine("Фиолетовый");
+            }
+            else if (Color <= 6)
+            {
+                Console.WriteLine("Синий");
+            }
+            else if(Color <= 9)
+            {
+                Console.WriteLine("Голубой");
+            }
+            else if (Color <= 12)
+            {
+                Console.WriteLine("Зеленый");
+            }
+            else if (Color <= 15)
+            {
+                Console.WriteLine("Желтый");
+            }
+            else if (Color <= 18)
+            {
+                Console.WriteLine("Оранжевый");
+            }
+            else if (Color <= 21)
+            {
+                Console.WriteLine("Красный");
+            }
+        }
+    }
+}
---
+++ TaskStar/Program.cs
@@ -0,0 +1,131 @@
+﻿namespace TaskStar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число соответсвующее желаемйо вам операции:\n" +
+                        "1.Сложение\n" +
+                        "2.Вычетание\n" +
+                        "3.Умноженние\n" +
+                        "4.Деление\n" +
+                        "5.Процент от числа\n" +
+                        "6.Квадратный корень числа\n" +
+                        "7.Вывести результат");
+            int func = Convert.ToInt32(Console.ReadLine());
+            double result=0;
+            double num1;
+            double num2;
+            switch (func) 
+            {
+                case 1:
+                    if (result == 0)
+                    {
+                        num1 = Convert.ToDouble(Console.ReadLine());
+                        num2 = Convert.ToDouble(Console.ReadLine());
+                        result = num1 + num2;
+                        goto case 8;
+                    }
+                    else {
+                        num1 = Convert.ToDouble(Console.ReadLine());
+                        result = num1 + result;
+                        goto case 8;
+                    }
+                case 2:
+                    if (result == 0)
+                    {
+                        num1 = Convert.ToDouble(Console.ReadLine());
+                        num2 = Convert.ToDouble(Console.ReadLine());
+                        result = num1 - num2;
+                        goto case 8;
+                    }
+                    else
+                    {
+                        num1 = Convert.ToDouble(Console.ReadLine());
+                        result = num1 - result;
+                        goto case 8;
+                    }
+                case 3:
+                    if (result == 0)
+                    {
+                        num1 = Convert.ToDouble(Console.ReadLine());
+                        num2 = Convert.ToDouble(Console.ReadLine());
+                        result = num1 * num2;
+                        goto case 8;
+                    }
+                    else
+                    {
+                        num1 = Convert.ToDouble(Console.ReadLine());
+                        result = num1 * result;
+                        goto case 8;
+                    }
+                case 4:
+                    if (result == 0)
+                    {
+                        num1 = Convert.ToDouble(Console.ReadLine());
+                        num2 = Convert.ToDouble(Console.ReadLine());
+                        result = num1 / num2;
+                        goto case 8;
+                    }
+                    else
+                    {
+                        num1 = Convert.ToDouble(Console.ReadLine());
+                        result = num1 / result;
+                        goto case 8;
+                    }
+                case 5:
+                    if (result == 0)
+                    {
+                        num1 = Convert.ToDouble(Console.ReadLine());
+                        num2 = Convert.ToDouble(Console.ReadLine());
+                        result = num2 / 100 * num1;
+                        goto case 8;
+                    }
+                    else
+                    {
+                        num1 = Convert.ToDouble(Console.ReadLine());
+                        result = result / 100 * num1;
+                        goto case 8;
+                    }
+                case 6:
+                    if (result == 0)
+                    {
+                        num1 = Convert.ToDouble(Console.ReadLine());
+                        result = Math.Sqrt(num1);
+                        goto case 8;
+                    }
+                    else
+                    {
+                        result = Math.Sqrt(result);
+                        goto case 8;
+                    }
+                case 7:
+                    Console.WriteLine($"{result}");
+                    goto case 8;
+                case 8:
+                    Console.WriteLine("Завершить программу: Y/N");
+                    char symb = char.Parse(Console.ReadLine());
+                    if (symb == 'Y')
+                    {
+                        goto default;
+                    }
+                    else 
+                    {
+                        Console.WriteLine("Выберите операцию:");
+                        int reoperation = Convert.ToInt32(Console.ReadLine());
+                        if (reoperation == 1) { goto case 1; }
+                        if (reoperation == 2) { goto case 2; }
+                        if (reoperation == 3) { goto case 3; }
+                        if (reoperation == 4) { goto case 4; }
+                        if (reoperation == 5) { goto case 5; }
+                        if (reoperation == 6) { goto case 6; }
+                        if (reoperation == 7) { goto case 7; }
+                    }
+                    break;
+                default:
+                    break;
+                   
+            }
+        }
+    }
+}
