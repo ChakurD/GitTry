d72ab385bf729fc9f0e6d822cc34283d142cdadc
ChakurD <cdgameboy98@gmail.com>
2023-02-05 18:03:43 +0300

    Update Program.cs

--- TaskStar/Program.cs
+++ TaskStar/Program.cs
@@ -4,105 +4,49 @@
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Введите число:");
+            double result = Convert.ToDouble(Console.ReadLine());
             Console.WriteLine("Введите число соответсвующее желаемйо вам операции:\n" +
                         "1.Сложение\n" +
                         "2.Вычетание\n" +
                         "3.Умноженние\n" +
                         "4.Деление\n" +
                         "5.Процент от числа\n" +
-                        "6.Квадратный корень числа\n" +
-                        "7.Вывести результат");
+                        "6.Квадратный корень числа");
             int func = Convert.ToInt32(Console.ReadLine());
-            double result=0;
             double num1;
-            double num2;
             switch (func) 
             {
                 case 1:
-                    if (result == 0)
-                    {
                         num1 = Convert.ToDouble(Console.ReadLine());
-                        num2 = Convert.ToDouble(Console.ReadLine());
-                        result = num1 + num2;
-                        goto case 8;
-                    }
-                    else {
-                        num1 = Convert.ToDouble(Console.ReadLine());
-                        result = num1 + result;
-                        goto case 8;
-                    }
+                        result = result + num1;
+                    Console.WriteLine($"Результат: {result}");
+                        goto case 7;
                 case 2:
-                    if (result == 0)
-                    {
                         num1 = Convert.ToDouble(Console.ReadLine());
-                        num2 = Convert.ToDouble(Console.ReadLine());
-                        result = num1 - num2;
-                        goto case 8;
-                    }
-                    else
-                    {
-                        num1 = Convert.ToDouble(Console.ReadLine());
-                        result = num1 - result;
-                        goto case 8;
-                    }
+                        result = result - num1;
+                    Console.WriteLine($"Результат: {result}");
+                    goto case 7;
                 case 3:
-                    if (result == 0)
-                    {
                         num1 = Convert.ToDouble(Console.ReadLine());
-                        num2 = Convert.ToDouble(Console.ReadLine());
-                        result = num1 * num2;
-                        goto case 8;
-                    }
-                    else
-                    {
-                        num1 = Convert.ToDouble(Console.ReadLine());
-                        result = num1 * result;
-                        goto case 8;
-                    }
+                        result = result * num1;
+                    Console.WriteLine($"Результат: {result}");
+                    goto case 7;
                 case 4:
-                    if (result == 0)
-                    {
                         num1 = Convert.ToDouble(Console.ReadLine());
-                        num2 = Convert.ToDouble(Console.ReadLine());
-                        result = num1 / num2;
-                        goto case 8;
-                    }
-                    else
-                    {
-                        num1 = Convert.ToDouble(Console.ReadLine());
-                        result = num1 / result;
-                        goto case 8;
-                    }
+                        result = result / num1;
+                    Console.WriteLine($"Результат: {result}");
+                    goto case 7;
                 case 5:
-                    if (result == 0)
-                    {
-                        num1 = Convert.ToDouble(Console.ReadLine());
-                        num2 = Convert.ToDouble(Console.ReadLine());
-                        result = num2 / 100 * num1;
-                        goto case 8;
-                    }
-                    else
-                    {
                         num1 = Convert.ToDouble(Console.ReadLine());
                         result = result / 100 * num1;
-                        goto case 8;
-                    }
+                    Console.WriteLine($"Результат: {result}");
+                    goto case 7;
                 case 6:
-                    if (result == 0)
-                    {
-                        num1 = Convert.ToDouble(Console.ReadLine());
-                        result = Math.Sqrt(num1);
-                        goto case 8;
-                    }
-                    else
-                    {
                         result = Math.Sqrt(result);
-                        goto case 8;
-                    }
+                    Console.WriteLine($"Результат: {result}");
+                    goto case 7;
                 case 7:
-                    Console.WriteLine($"{result}");
-                    goto case 8;
-                case 8:
                     Console.WriteLine("Завершить программу: Y/N");
                     char symb = char.Parse(Console.ReadLine());
                     if (symb == 'Y')
@@ -119,7 +63,6 @@
                         if (reoperation == 4) { goto case 4; }
                         if (reoperation == 5) { goto case 5; }
                         if (reoperation == 6) { goto case 6; }
-                        if (reoperation == 7) { goto case 7; }
                     }
                     break;
                 default:
