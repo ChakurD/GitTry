6e4c04d24b1102e4f53b4a23e0cea94bb4128996
ChakurD <cdgameboy98@gmail.com>
2023-02-15 20:45:49 +0300

    OOP№1

---
+++ startHWClass/startHWClass/Phone.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace startHWClass
+{
+    public class Phone
+    {
+        public string number;
+        public string model;
+        public int weight;
+        
+        public void ReceiveCall(string name)
+        {
+            Console.WriteLine($"Звонит {name}");
+        }
+        public void GetNumber()
+        {
+            Console.WriteLine($"{number}");
+        }
+        public void GetCharacteristics() 
+        {
+            Console.WriteLine($"Номер: {number}; Модель: {model}; Вес: {weight};");
+        }
+        public Phone(long numberInit, string modelInit, int weightInit) 
+        {
+        number = string.Format("{0:# (###) ###-##-##}",numberInit);
+         model = modelInit;
+         weight = weightInit;
+        }
+        public Phone(long numberInit, string modelInit)
+        { 
+        number= string.Format("{0:# (###) ###-##-##}", numberInit);
+        model= modelInit;
+        }
+        public Phone()
+        { }
+    }
+}
---
+++ startHWClass/startHWClass/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace startHWClass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите характеристики телефонов(Номер, Модель , Вес):");
+            string characteristics = Console.ReadLine();
+            string[] characteristicsSplit = characteristics.Split(' ',',','\\','.');
+
+            long iphNumber = Convert.ToInt64(characteristicsSplit[0]);
+            string iphModel = characteristicsSplit[1];
+            int iphWeight = Convert.ToInt32(characteristicsSplit[2]);
+            long samsNumber = Convert.ToInt64(characteristicsSplit[3]);
+            string samsModel = characteristicsSplit[4];
+
+
+            Phone iphone = new Phone(iphNumber, iphModel, iphWeight);
+            Phone samsung = new Phone(samsNumber, samsModel);
+            Phone huawei = new Phone();
+
+            samsung.weight = Convert.ToInt32(characteristicsSplit[5]);
+
+            huawei.number= string.Format("{0:# (###) ###-##-##}", Convert.ToInt64(characteristicsSplit[6]));
+            huawei.model = characteristicsSplit[7];
+            huawei.weight = Convert.ToInt32(characteristicsSplit[8]);
+            
+            
+            iphone.GetCharacteristics();
+            samsung.GetCharacteristics();
+            huawei.GetCharacteristics();
+
+            string nameHuawei = "Alex";
+            string nameIph = "Volodya";
+            string nameSams = "Andrey";
+
+            iphone.ReceiveCall(nameHuawei);
+            samsung.ReceiveCall(nameIph);
+            huawei.ReceiveCall(nameSams);
+
+            iphone.GetNumber();
+            samsung.GetNumber();
+            huawei.GetNumber();
+        }
+    }
+}
