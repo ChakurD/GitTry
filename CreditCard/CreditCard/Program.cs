b372e902ba7f2265170c5d027f75b081fd6adf30
ChakurD <cdgameboy98@gmail.com>
2023-02-19 20:25:06 +0300

    CreditCardClass

---
+++ CreditCard/CreditCard/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace CreditCard
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CreditCard masterCardUITBank = new CreditCard();
+            CreditCard visaCardAPBank = new CreditCard();
+            CreditCard masterCardHoverBank = new CreditCard();
+
+            masterCardUITBank.AccrualAmount();
+            visaCardAPBank.AccrualAmount();
+            masterCardHoverBank.DebitAmount();
+
+            masterCardUITBank.InformAbCrCard();
+            visaCardAPBank.InformAbCrCard();
+            masterCardHoverBank.InformAbCrCard();
+        }
+    }
+}
