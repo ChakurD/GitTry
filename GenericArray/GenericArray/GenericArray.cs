d541ee8f5b960a08efca3ba7da462b7dd98d6cb8
ChakurD <cdgameboy98@gmail.com>
2023-03-05 18:35:21 +0300

    Generic

---
+++ GenericArray/GenericArray/GenericArray.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericArray
+{
+    public class GenericArray<T>
+    {
+        private T[] array;
+        private int fillIndex;
+        public GenericArray() 
+        {
+            array = new T[10];
+            fillIndex = 0;
+        }
+        public void AddElem(T elem) 
+        {
+            try
+            {
+                array.SetValue(elem, fillIndex);
+                fillIndex++;
+            }
+            catch (IndexOutOfRangeException) { Console.WriteLine("fillIndex не может быть больше 10");}
+        }
+        public T GetEllement(int index) 
+        {
+         return (T)array.GetValue(index);
+        }
+        public void CountElements()
+        {
+            Console.WriteLine($"Количество элементов в массиве: {fillIndex}");
+        }
+    }
+}
