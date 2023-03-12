0a70d0a637cd6ee29d43903eca394e4ab483dada
ChakurD <cdgameboy98@gmail.com>
2023-03-12 13:12:21 +0300

    Delegate_Serialization

---
+++ Lesson12-13/DelegateHomeWork/DelegateHomeWork/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace DelegateHomeWork
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DaysOfTheWeekAndWeather days= new DaysOfTheWeekAndWeather();
+            DaysOfTheWeekAndWeatherDelegate week = days.WhatADay;
+            week();
+            week();
+            week();
+            week();
+            week();
+            week();
+            week();
+            week();
+            week();
+            week();
+            week();
+            week();
+
+        }
+    }
+}
---
+++ Lesson12-13/DeserializeSquad/DeserializeSquad/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Numerics;
+using System.Text.Json;
+
+namespace DeserializeSquad
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var fs = new FileStream("data.json", FileMode.Open))
+            {
+                var superHeroSquad = JsonSerializer.Deserialize<Squad>(fs);
+                Console.WriteLine($"Имя отряда: {superHeroSquad.squadName}\nГород действия: {superHeroSquad.homeTown}\nДата основания: {superHeroSquad.formed}\nСекретная база: {superHeroSquad.secretBase}\nАктивность: {superHeroSquad.active}");
+                Console.WriteLine("Члены отряда:");
+                superHeroSquad.ShowMemebrs();
+
+            }
+        }
+    }
+}
