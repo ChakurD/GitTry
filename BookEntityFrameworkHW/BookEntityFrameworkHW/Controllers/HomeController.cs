43f31b9f721b6a95cb3ece1b42c6c2517a893261
ChakurD <cdgameboy98@gmail.com>
2023-04-19 20:14:52 +0300

    EntityCopy

---
+++ BookEntityFrameworkHW/BookEntityFrameworkHW/Controllers/HomeController.cs
@@ -0,0 +1,78 @@
+﻿using BookEntityFrameworkHW.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace BookEntityFrameworkHW.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private ApplicationContext db;
+
+        public HomeController(ILogger<HomeController> logger, ApplicationContext context)
+        {
+            _logger = logger;
+            db = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            var books = db.Books.ToList();
+            return View(books);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Book result)
+        {
+            db.Books.Add(result);
+            db.SaveChanges();
+            return RedirectToAction("Privacy");
+        }
+        public IActionResult Edit(int? id)
+        {
+            Book book = db.Books.FirstOrDefault(p => p.Id == id);
+            if (book != null)
+                return View(book);
+
+            return NotFound();
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Book user)
+        {
+            db.Books.Update(user);
+            db.SaveChanges();
+            return RedirectToAction("Privacy");
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            Book book = db.Books.FirstOrDefault(p => p.Id == id);
+            if (book != null)
+            {
+                db.Books.Remove(book);
+                db.SaveChanges();
+                return RedirectToAction("Privacy");
+            }
+            return NotFound();
+        }
+
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ BookEntityFrameworkHW/BookEntityFrameworkHW/Models/ApplicationContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace BookEntityFrameworkHW.Models
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+        public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Book>().HasData(
+                new Book { Id = 1, Title = "Algebra", CreatedDate = DateTime.Parse("12 Jan 2007"), AuthorName = "Matews", Description = "Description 1" },
+                new Book { Id = 2, Title = "Jupiter", CreatedDate = DateTime.Parse("25 Jul 2097"), AuthorName = "Gromov", Description = "Description 2" },
+                new Book { Id = 3, Title = "Countrys", CreatedDate = DateTime.Parse("7 Feb 2008"), AuthorName = "Petrov", Description = "Description 3" },
+                new Book { Id = 4, Title = "Stroy of New World", CreatedDate = DateTime.Parse("23 Sep 2013"), AuthorName = "Cicirov", Description = "Description 4" },
+                new Book { Id = 5, Title = "Yourself", CreatedDate = DateTime.Parse("30 Nov 2024"), AuthorName = "Jonathan", Description = "Description 5" }
+                );
+        }
+    }
+}
